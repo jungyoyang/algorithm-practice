e89b14ce58106ac10506658670b74d664817cb43
Jungyoyang <2jungyo@naver.com>
2025-06-15 20:05:40 +0900

    Add Solution.java for valid parentheses problem

---
+++ leetcode/Valid Parentheses/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution {
+        public boolean isValid(String s) {
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();             // 한 줄 전체 입력받기
+            if (s != (){}[]) //아닐경우 -1로 밖으로 뺴자
+            for(i=0;i<s.length;i++){
+                for(j=0;j<s.length;j++){
+                    if (s[i] =='('&& j>i &&s[j] ==')')
+                }
+            }
+        }
+    }return new s
