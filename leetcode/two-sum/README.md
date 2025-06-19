3edae8c4a6936684bd17391d7cdfe294033bc119
Jungyo Yang(Window) <2jungyo@naver.com>
2025-06-19 21:01:57 +0900

    해쉬맵 공부

--- leetcode/two-sum/README.md
+++ leetcode/two-sum/README.md
@@ -21,3 +21,22 @@ return null; // 두 수를 못 찾았을 때를 위한 대비
 
 
 return new int[] {i,j}; //new가 들어가야함
+
+
+
+You may assume that each input would have exactly one solution." 이 조건은 뭔 말일까
+
+📘 해석:
+입력은 반드시 하나의 정답만 존재한다고 가정해도 좋습니다.
+
+즉, 어떤 nums 배열과 target 값이 주어지면,
+딱 하나의 정답 조합만 존재해도된다 뜻이다
+
+🧠 의미:
+중복된 정답을 고려하지 않아도 된다
+
+예외 처리나 여러 해를 찾는 코드를 쓸 필요가 없다.
+
+정답을 찾는 즉시 return 해도 괜찮다.
+
+----
--- leetcode/two-sum/Solution_wrong.java
+++ leetcode/two-sum/Solution_wrong.java
@@ -1,17 +1,24 @@
+import java.util.HashMap;
+import java.util.Map; // 인터페이스
+
+Map<i, j> hash = new HashMap<>();
+int i;
+int j;
+int target;
+i + j = target
+map.put("apple", "사과");
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) { //j는 i와 같지않다는 조건도 써야하는데..
-                if(nums[i] + nums[j]==target) {
+            for(int j = 0; j < nums.length; j++) { 
+                if(nums[i] + nums[j]==target && i != j) {
                     return new int[] {i,j};
                 }
             }
-            //You may assume that each input would have exactly one solution 이조건도 모르겠음.
+               
+
         }
+        return new int[] {-1, -1};
     }
-    
-    
-    
-
-
 }
