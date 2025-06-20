f7687fed27ecb70e981f6da3ec2367256c20ef42
Jungyoyang <2jungyo@naver.com>
2025-06-21 00:45:30 +0900

    fix: 수정된 두 수 합 문제의 해시맵 초기화 및 불필요한 코드 제거

--- leetcode/two-sum/Solution_wrong.java
+++ leetcode/two-sum/Solution_wrong.java
@@ -1,25 +1,11 @@
 import java.util.HashMap;
 import java.util.Map; // 인터페이스
 
-Map<int, int> hash = new HashMap<>();
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>(); // 제네릭안에는 레퍼런스 타입만.
 int i;
 int j;
 int target;
 target = i + j;
 map.put(i, j);
-
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) { 
-                if(nums[i] + nums[j]==target && i != j) {
-                    return new int[] {i,j};
-                }
-            }
-               
-
-        }
-        return new int[] {-1, -1};
-    }
-}
