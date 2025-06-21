a1c3b48f5c4fc7a0571792da13a73d706f4b227a
Jungyo Yang(Window) <2jungyo@naver.com>
2025-06-21 23:56:42 +0900

    fix: for 루프 구문 추가 및 변수 선언 위치 수정

--- leetcode/two-sum/Solution_wrong.java
+++ leetcode/two-sum/Solution_wrong.java
@@ -4,6 +4,10 @@ import java.util.Map; // 인터페이스
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hash = new HashMap<>(); // 제네릭안에는 레퍼런스 타입만.
+
+        for(i=0;i<nums.length;i++) {
+            
+        }
 int i;
 int j;
 int target;
